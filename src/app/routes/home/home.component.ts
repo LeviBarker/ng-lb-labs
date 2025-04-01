a58581633d2c3d30bbeb4dba86042bce8ea214f2
Levi Barker <levi.barker@outlook.com>
2025-04-01 07:51:24 -0500

    Add Google sign-in UI, Firebase emulators, and key combo logic
    Implemented a custom Google sign-in button with a new CSS style and updated the home component to include password sign-in and reactive key combination detection. Added Firebase emulator configurations for auth and Firestore, enhancing local development workflows. Improved UI organization with MatCard and MatDivider integration.

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -2,13 +2,16 @@ import {Component, inject} from '@angular/core';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatButton, MatIconButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
-import {Auth, GoogleAuthProvider, signInWithPopup} from '@angular/fire/auth';
+import {Auth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup} from '@angular/fire/auth';
 import {LoginStore} from '../../slices/login/login.store';
-import {JsonPipe} from '@angular/common';
+import {AsyncPipe, JsonPipe} from '@angular/common';
+import {bufferCount, fromEvent, map} from 'rxjs';
+import {MatCard, MatCardContent} from '@angular/material/card';
+import {MatDivider} from '@angular/material/divider';
 
 @Component({
   selector: 'app-home',
-  imports: [MatToolbar, MatButton, MatIcon, MatIconButton, JsonPipe],
+  imports: [MatToolbar, MatButton, MatIcon, JsonPipe, AsyncPipe, MatCard, MatCardContent, MatDivider],
   templateUrl: './home.component.html',
   standalone: true,
   styleUrl: './home.component.css'
@@ -18,10 +21,28 @@ export class HomeComponent {
   auth = inject(Auth);
   loginStore = inject(LoginStore);
 
+  pressedKeys$ = fromEvent<KeyboardEvent>(window, 'keydown').pipe(
+    map((event) => event.key),
+    bufferCount(10),
+    map((keys) => keys.join(','))
+  )
+
+  secretCodeEntered$ = this.pressedKeys$.pipe(
+    map((keys) => keys == 'ArrowUp,ArrowUp,ArrowDown,ArrowDown,ArrowLeft,ArrowRight,ArrowLeft,ArrowRight,a,b')
+  )
+
   async continueWithGoogle() {
     try {
       const provider = new GoogleAuthProvider();
       await signInWithPopup(this.auth, provider);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async signInWithEmailAndPassword(email: string, password: string) {
+    try {
+      await signInWithEmailAndPassword(this.auth, email, password);
     } catch (err) {
       console.error(err);
     }
